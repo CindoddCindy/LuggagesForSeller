f32a01ead8e6403518fe2a67d2352e90eca642cc
CindoddCindy <cindodcindy@gmail.com>
2021-04-23 09:03:09 +0700

    tambah layout dan kelas kelas di view customer

---
+++ _nitipAplicationPembeli/app/src/main/java/com/cindodcindy/nitippembeli/view/BottomNavActivity.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.nitippembeli.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.nitippembeli.R;
+
+public class BottomNavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav);
+    }
+}
