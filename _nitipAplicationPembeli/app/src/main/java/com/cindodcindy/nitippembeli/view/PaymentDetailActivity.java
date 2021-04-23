adb42bd01e57dd5c173103d3acda9ecbe7bedbf9
CindoddCindy <cindodcindy@gmail.com>
2021-04-23 10:08:16 +0700

    tambah kelas2 layout activity

---
+++ _nitipAplicationPembeli/app/src/main/java/com/cindodcindy/nitippembeli/view/PaymentDetailActivity.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.nitippembeli.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.nitippembeli.R;
+
+public class PaymentDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_detail);
+    }
+}
