6d244797a5caeefc2f63544f88a86e719c45205b
CindoddCindy <cindodcindy@gmail.com>
2021-04-28 15:16:17 +0700

    tambah booking view

--- _nitipAplicationPembeli/app/src/main/java/com/cindodcindy/nitippembeli/view/BookingDetailActivity.java
+++ _nitipAplicationPembeli/app/src/main/java/com/cindodcindy/nitippembeli/view/BookingDetailActivity.java
@@ -3,14 +3,60 @@ package com.cindodcindy.nitippembeli.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.cindodcindy.nitippembeli.R;
 
 public class BookingDetailActivity extends AppCompatActivity {
 
+    //data jasa
+    private TextView textView_asal, textView_tujuan, textView_date_going, textView_date_arive,
+            textView_time_going, textView_time_arrive, textView_nama_penjual, textView_harga, textView_jenis_barang, textView_kapasitas;
+
+
+    //data buyer
+
+    private TextView textView_by_asal, textView_by_tujuan, textView_by_pengirim, textView_by_penerima, textView_by_jenis_barang, textView_by_berat_brg;
+
+
+
+    //btn
+
+    private  TextView textView_btn_edit_kirim_booking, textView_btn_hapus_booking, textView_btn_back_dari_booking;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_detail);
+
+        //data jasa
+
+        textView_asal=findViewById(R.id.id_tv_book_det_lugg_asal);
+        textView_tujuan=findViewById(R.id.tv_book_det_lugg_tujuan);
+        textView_date_going=findViewById(R.id.tv_book_det_lugg_tgal_going);
+        textView_date_arive=findViewById(R.id.tv_book_det_lugg_tgal_arr);
+        textView_time_going=findViewById(R.id.tv_book_det_lugg_time_going);
+        textView_time_arrive=findViewById(R.id.tv_book_det_lugg_time_arr);
+        textView_nama_penjual=findViewById(R.id.tv_book_det_lugg_nama_seller);
+        textView_harga=findViewById(R.id.tv_book_det_lugg_price);
+        textView_jenis_barang=findViewById(R.id.tv_book_det_lugg_type);
+        textView_kapasitas=findViewById(R.id.tv_book_det_lugg_height);
+
+        //data pembeli
+
+        textView_by_asal=findViewById(R.id.tv_book_det_by_asal);
+        textView_by_tujuan=findViewById(R.id.tv_book_det_by_tujuan);
+        textView_by_pengirim=findViewById(R.id.tv_book_det_by_pengirim);
+        textView_by_penerima=findViewById(R.id.tv_book_det_by_penerima);
+        textView_by_jenis_barang=findViewById(R.id.tv_book_det_by_jenis_barang);
+        textView_by_berat_brg=findViewById(R.id.tv_book_det_by_berat_barang);
+
+        textView_btn_edit_kirim_booking=findViewById(R.id.tv_book_det_btn_edit_booking);
+        textView_btn_hapus_booking=findViewById(R.id.tv_book_det_btn_hapus_booking);
+        textView_btn_back_dari_booking=findViewById(R.id.tv_book_det_btn_back);
+
     }
 }
--- _nitipAplicationPembeli/app/src/main/java/com/cindodcindy/nitippembeli/view/adapter/BookingAdapter.java
+++ _nitipAplicationPembeli/app/src/main/java/com/cindodcindy/nitippembeli/view/adapter/BookingAdapter.java
@@ -2,9 +2,13 @@ package com.cindodcindy.nitippembeli.view.adapter;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.cindodcindy.nitippembeli.R;
 
 public class BookingAdapter  extends RecyclerView.Adapter<BookingAdapter.BookingChild> {
 
@@ -26,8 +30,25 @@ public class BookingAdapter  extends RecyclerView.Adapter<BookingAdapter.Booking
 
     public class BookingChild  extends RecyclerView.ViewHolder{
 
+        private TextView textView_asal, textView_tujuan, textView_date_going, textView_date_arive,
+                textView_time_going, textView_time_arrive, textView_nama_penjual, textView_harga, textView_jenis_barang, textView_kapasitas;
+
+        private CardView cardView_booking_adapter;
+
         public BookingChild(@NonNull View itemView) {
             super(itemView);
+            textView_asal=itemView.findViewById(R.id.id_tv_book_item_lugg_asal);
+            textView_tujuan=itemView.findViewById(R.id.tv_book_item_lugg_tujuan);
+            textView_date_going=itemView.findViewById(R.id.tv_book_item_lugg_tgal_going);
+            textView_date_arive=itemView.findViewById(R.id.tv_book_item_lugg_tgal_arr);
+            textView_time_going=itemView.findViewById(R.id.tv_book_item_lugg_time_going);
+            textView_time_arrive=itemView.findViewById(R.id.tv_book_item_lugg_time_arr);
+            textView_nama_penjual=itemView.findViewById(R.id.tv_book_item_lugg_nama_seller);
+            textView_harga=itemView.findViewById(R.id.tv_book_item_lugg_price);
+            textView_jenis_barang=itemView.findViewById(R.id.tv_book_item_lugg_type);
+            textView_kapasitas=itemView.findViewById(R.id.tv_book_item_lugg_height);
+
+            cardView_booking_adapter=itemView.findViewById(R.id.cv_book_det_lugg_av);
         }
     }
 }
