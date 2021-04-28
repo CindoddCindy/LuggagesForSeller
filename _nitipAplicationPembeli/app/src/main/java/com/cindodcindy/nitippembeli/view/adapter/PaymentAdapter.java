6a26552cdee46ca4305c4b5063a3a0a87539c698
CindoddCindy <cindodcindy@gmail.com>
2021-04-28 15:55:53 +0700

    tambah payment adapter:

--- _nitipAplicationPembeli/app/src/main/java/com/cindodcindy/nitippembeli/view/adapter/PaymentAdapter.java
+++ _nitipAplicationPembeli/app/src/main/java/com/cindodcindy/nitippembeli/view/adapter/PaymentAdapter.java
@@ -2,9 +2,13 @@ package com.cindodcindy.nitippembeli.view.adapter;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.cindodcindy.nitippembeli.R;
 
 public class PaymentAdapter extends RecyclerView.Adapter<PaymentAdapter.PaymentChild> {
 
@@ -26,8 +30,21 @@ public class PaymentAdapter extends RecyclerView.Adapter<PaymentAdapter.PaymentC
 
     public  class PaymentChild extends RecyclerView.ViewHolder{
 
+        private TextView textView_sl_nama_penerima, textView_jumlah_uang, textView_tgl_tf,textView_sl_nama_bank;
+
+        private CardView cardView_pay;
+
+
         public PaymentChild(@NonNull View itemView) {
             super(itemView);
+
+            textView_sl_nama_penerima=itemView.findViewById(R.id.tv_pay_rv_nama_pengirim);
+            textView_jumlah_uang=itemView.findViewById(R.id.tv_pay_rv_jumlah_uang);
+            textView_tgl_tf=itemView.findViewById(R.id.tv_pay_rv_tgal_tf);
+            textView_sl_nama_bank=itemView.findViewById(R.id.tv_pay_rv_nama_bank);
+
+
+            cardView_pay=itemView.findViewById(R.id.cv_payment);
         }
     }
 }
