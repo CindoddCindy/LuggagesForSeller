2583f53da10420b882580f2c2aeb244ae733f835
CindoddCindy <cindodcindy@gmail.com>
2021-05-01 18:50:32 +0700

    tambah booking adapter

--- _nitipAplicationPembeli/app/src/main/java/com/cindodcindy/nitippembeli/view/adapter/BookingAdapter.java
+++ _nitipAplicationPembeli/app/src/main/java/com/cindodcindy/nitippembeli/view/adapter/BookingAdapter.java
@@ -1,5 +1,9 @@
 package com.cindodcindy.nitippembeli.view.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,23 +13,83 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cindodcindy.nitippembeli.R;
+import com.cindodcindy.nitippembeli.model.pojo_booking.pojo_get_booking.Content;
+import com.cindodcindy.nitippembeli.view.BookingDetailActivity;
+import com.cindodcindy.nitippembeli.view.KonfirmDetailActivity;
+
+import java.util.List;
 
 public class BookingAdapter  extends RecyclerView.Adapter<BookingAdapter.BookingChild> {
+
+    public List<com.cindodcindy.nitippembeli.model.pojo_booking.pojo_get_booking.Content> contentList;
+    public Context context;
+
+    public  BookingAdapter(List<Content> contentList, Context context){
+        this.contentList=contentList;
+        this.context=context;
+
+    }
 
     @NonNull
     @Override
     public BookingChild onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.booking_item_layout, parent, false);
+        BookingChild mViewHolder = new BookingChild(mView);
+
+
+        return mViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull BookingChild holder, int position) {
+        final Content content= contentList.get(position);
+        holder.textView_asal.setText(content.getAsal());
+        holder.textView_tujuan.setText(content.getTujuan());
+        holder.textView_date_going.setText(content.getTanggalBerangkat());
+        holder.textView_date_arive.setText(content.getTanggalTiba());
+        holder.textView_time_going.setText(content.getJamBerangkat());
+        holder.textView_time_arrive.setText(content.getJamTiba());
+        holder.textView_nama_penjual.setText(content.getNamaPenjual());
+        holder.textView_harga.setText(content.getHargaBagasi());
+        holder.textView_kapasitas.setText(content.getKapasitas());
+        holder.textView_jenis_barang.setText(content.getJenisBarang());
+
+        holder.cardView_booking_adapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putLong("id_seller", content.getIdSeller());
+                bundle.putString("asal",content.getAsal());
+                bundle.putString("tujuan",content.getTujuan());
+                bundle.putString("tglgo",content.getTanggalBerangkat());
+                bundle.putString("tglarr",content.getTanggalTiba());
+                bundle.putString("jamgo", content.getJamBerangkat());
+                bundle.putString("jamarr",content.getJamTiba());
+                bundle.putString("namapenjual",content.getNamaPenjual());
+                bundle.putString("kapasitas",content.getKapasitas());
+                bundle.putString("jenisbarang",content.getJenisBarang());
+                bundle.putString("harga", content.getHargaBagasi());
+
+                bundle.putString("asalBr",content.getAlamatPembeli());
+                bundle.putString("tujuanBr",content.getAlamatPenerima());
+                bundle.putString("pengirim",content.getNamaPembeli());
+                bundle.putString("penerima",content.getNamaPenerima());
+                bundle.putString("jenisBr", content.getJenisBarangKirim());
+                bundle.putString("beratBr",content.getKapasitasBarang());
+
+                Intent intent = new Intent(context, BookingDetailActivity.class);
+                intent.putExtras(bundle);
+                context.startActivity(intent);
+
+            }
+        });
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return contentList.size();
     }
 
     public class BookingChild  extends RecyclerView.ViewHolder{
